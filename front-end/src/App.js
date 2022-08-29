950a4e9a4f099e2dd101022c93da763149aa0ccb
JuHaoChen1997 <juhaochen97@gmail.com>
2022-08-29 15:26:19 -0400

    add frontend new route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Index from "./pages/Index";
 import Show from "./pages/Show";
+import New from "./pages/New";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Index />} />
         <Route path="/recipes/:recipeId" element={<Show />} />
+        <Route path="/recipes/new" element={<New />} />
       </Routes>
     </div>
   );
---
+++ front-end/src/pages/New.js
@@ -0,0 +1,12 @@
+import RecipeNewForm from "../Components/RecipeNewFom";
+
+function New() {
+  return (
+    <div>
+      <h2>New</h2>
+      <RecipeNewForm />
+    </div>
+  );
+}
+
+export default New;
