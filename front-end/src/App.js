3f3a4fc8d762d2dd6907b8f1d9962eb21b3ac293
JuHaoChen1997 <juhaochen97@gmail.com>
2022-08-29 15:47:17 -0400

    add frontend error route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Index from "./pages/Index";
 import Show from "./pages/Show";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
+import FourOFour from "./pages/FourOFour";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/recipes/:recipeId" element={<Show />} />
         <Route path="/recipes/new" element={<New />} />
         <Route path="/recipes/:recipeId/edit" element={<Edit />} />
+        <Route path="*" element={<FourOFour />} />
       </Routes>
     </div>
   );
